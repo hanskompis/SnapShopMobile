0ed8ef23fa0215da67fd760a6b9b8de208db18e1
Anni M Tuulinen <anni.tuulinen@helsinki.fi>
2012-05-22 11:54:46 +0300

    browse table

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -1,3 +1,5 @@
 // SnapShopMobile global configuration file
 
 var backendUrl = "http://test.moremr.com/snapshop/api/v1/";
+var browseTableRow = 4;
+var browseTableCol = 3;
--- assets/www/js/views/browse-view.js
+++ assets/www/js/views/browse-view.js
@@ -2,7 +2,15 @@ var BrowseView = Backbone.View.extend({
    render: function() {
      var browseElement = Mustache.to_html($("#browse-template").html(), {});
      $(this.el).html(browseElement);
-     alert(document.cookie);
+//     alert(document.cookie);
+     for(var i = 0; i < browseTableRow; i++){
+    	 var rowElement = Mustache.to_html($("#browse-table-row-template").html(), {});
+    	 $("#pictureTable").append(rowElement);
+    	 for(var j = 0; j < browseTableCol; j++){
+    		 var dataElement = Mustache.to_html($("#browse-table-data-template").html(), {});
+    		 $("tr:last").append(dataElement); 
+    	 }
+     }
    },
 
 	   
