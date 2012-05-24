247849191a546da6d7e0de59293f46729ce58bee
Anni M Tuulinen <anni.tuulinen@helsinki.fi>
2012-05-24 11:01:51 +0300

    previous and next buttons

--- assets/www/js/views/browse-view.js
+++ assets/www/js/views/browse-view.js
@@ -1,5 +1,6 @@
 var BrowseView = Backbone.View.extend({
     myPictures: true,
+    offset: 0,
 	
     render: function() {
 	    if(this.myPictures) {
@@ -17,18 +18,13 @@ var BrowseView = Backbone.View.extend({
 //			getItemsService.getItemsCollection("organization", 0, 12, "1"); 
 //		}
 		
-		//getItemsCollection : function (listType, offset, count, userID
-		getItemsService.getItemsCollection("organization", 0, 12, "1"); //TODO: check hard coded organization
+		getItemsService.getItemsCollection("organization", this.offset, 12, "1"); //TODO: check hard coded organization
 		getItemsService.onItemsFetched = function(items) {
-//			alert(items.length);
 //			alert(JSON.stringify(items));
-//
-//			
+
 //			items.forEach(function(item) {
 //				alert("id: " + item.get("id"));
 //			});
-
-//			alert(JSON.stringify(items[0]));
 			var count = items.length;
 	        var appended = 0;
 	        for(var i = 0; i < browseTableRow && appended < count; i++){
@@ -44,68 +40,40 @@ var BrowseView = Backbone.View.extend({
 
 		};
 		
-
-		
-//        alert(document.cookie);
-//        var count = this.collection.length;
-//        var count = 12; 
-//        var appended = 0;
-//        for(var i = 0; i < browseTableRow && appended < count; i++){
-//  	        var rowElement = Mustache.to_html($("#browse-table-row-template").html(), {});
-//  	        $("#pictureTable").append(rowElement);
-//  	        for(var j = 0; j < browseTableCol && appended < count; j++){
-//  	    	    var dataElement = Mustache.to_html($("#browse-table-data-template").html(), {});
-//  		        $("tr:last").append(dataElement); 
-//  		        appended++;
-//  	        }
-//        }
-		
      }, //render
      
      
      
-     
-
-     
      events: {
 	   "click #myImagesButton": "myImagesAction",
-       "click #allImagesButton": "allImagesAction"
+       "click #allImagesButton": "allImagesAction",
+       "click #nextImagesButton": "nextImagesAction",
+       "click #previousImagesButton": "previousImagesAction"
      },
     
      myImagesAction: function() {
+    	 this.offset = 0;
     	 this.myPictures = true;
     	 this.render(); 
      },
     
      allImagesAction: function() {
+    	 this.offset = 0;
     	 this.myPictures = false;
          this.render();
+     },
+     
+     nextImagesAction: function() {
+    	 this.offset += 12;
+         this.render();
+     },
+     
+     previousImagesAction: function() {
+    	 if(this.offset === 0)
+    		 return;
+    	 this.offset -= 12;
+         this.render();
      }
-
-   
-   // For getItemService testing
-   //
-   //
-//   events : {
-//	   "click #browseButton" : "browseAction"
-//   },
-//   browseAction : function() {
-//	   var cookieName = "snapcookie";
-//		var begin = document.cookie.indexOf(cookieName)
-//				+ cookieName.length + 1;
-//		var end = document.cookie.indexOf(";", begin);
-
-//		var getItemsService = new GetItemsService();
-//		getItemsService.onItemsFetched = function(items) {
-//			alert(items.length);
-//		};
-//		this.coll =  getItemsService.getItemsCollection("organization", 0, 11, "1");
-//   }
-
-
-   //
-   //
-   // For getItemService testing
 
 	   
 });
