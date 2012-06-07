39042344ad1c4727166975040f12682f3d9365ea
Markus K Koski <mkoski@dhp-75.cs.helsinki.fi>
2012-06-07 11:10:34 +0300

    uploadView testing

--- assets/www/js/views/upload-view.js
+++ assets/www/js/views/upload-view.js
@@ -1,18 +1,34 @@
 App.Views.UploadView = Backbone.View.extend({
    render: function() {
 	 
+	 var self = this;
 	 var uploadElement = Mustache.to_html($("#upload-picture-template").html(), {});
 	 $(this.el).html(uploadElement);
 
 	 App.categories.each(function(category) {
-		var categoryItem = category.get("name");		
-		alert(category.get("name"));
+		
+		var categoryName = category.get("name");
+		var categoryId = category.get("id");
+		//alert(category.get("name"));
+		var selectElement = Mustache.to_html($("#upload-picture-select").html(), {
+			categoryTitle : categoryName,
+			selectID : categoryId
+		});
+		$(self.el).append(selectElement);
 		var subcategoryItem = category.get("subcategories");
 		$.each(subcategoryItem, function(index, subcategory){			
-			alert(subcategory.name);
+			var subcategoryName = subcategory.name;
+			var optionElement = Mustache.to_html($("#upload-picture-option").html(), {
+				optionName : subcategoryName
+			});
+			$("#"+categoryID).append(optionElement);
 		}),
+
 	 });
+	 var uploadButtonElement = Mustache.to_html($("#upload-picture-button").html(), {});
+	 $(self.el).append(uploadButtonElement);
    },
+   
    
    events: {
 	   "click #uploadButton": "uploadAction",
