9e99ed34bcc475da4525d2070481f1a3d28e7405
Jari M J Koskinen <jarikosk@dhp-74.cs.helsinki.fi>
2012-06-07 10:09:28 +0300

    functions mod.

--- assets/www/js/camerafunctions.js
+++ assets/www/js/camerafunctions.js
@@ -1,30 +1,3 @@
-function uploadPhoto(imageURI) {
-    var options = new FileUploadOptions();
-    options.fileKey="file";
-    options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
-    options.mimeType="image/jpeg";
-
-    var params = new Object();
-    params.title = $("#imageTitle").val();
-    params.description = $("#imageDescription").val();
-
-    options.params = params;
-
-    var ft = new FileTransfer();
-    ft.upload(imageURI, backendUrl + "content", uploadSuccess, uploadFail, options);
-}
-
-function uploadSuccess(r) {
-    alert("Code = " + r.responseCode);
-    alert("Response = " + r.response);
-    alert("Sent = " + r.bytesSent);
-    App.picturePath = null;
-}
-
-function uploadFail(error) {
-    alert("An error has occurred: Code = " = error.code);
-}
-
 function takePic() {
 	alert("take pic");
 navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
