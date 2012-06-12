53e5502530b3bf43ba82a599eea91d9382c99e77
Markus K Koski <mkoski@dhp-76.cs.helsinki.fi>
2012-06-12 13:50:15 +0300

    login-view.js refactored

--- assets/www/js/views/login-view.js
+++ assets/www/js/views/login-view.js
@@ -1,21 +1,18 @@
 App.Views.LoginView = Backbone.View.extend({
-   render: function() {
-     var loginElement = Mustache.to_html($("#login-template").html(), {});
-     $(this.el).html(loginElement);
-   },
-   events: {
-	   "click #loginButton": "loginAction"
-   },
-   loginAction: function() {
-	   var sessionService = new SessionService();
-	   sessionService.loginOk = function(response) {
-		   document.cookie = response.session.id;
-		   userSession.authentication = response;	   
-		   Backbone.history.navigate("browse", true);
-	   };
-
-	   sessionService.makeLogin($("#loginUsername").val(), $("#loginPassword").val());
-
-   }
-	   
+    events: {
+        "click #loginButton": "loginAction"
+    },
+    render: function() {
+        var loginElement = Mustache.to_html($("#login-template").html(), {});
+        $(this.el).html(loginElement);
+    },
+    loginAction: function() {
+        var sessionService = new SessionService();
+        sessionService.loginOk = function(response) {
+            document.cookie = response.session.id;
+            userSession.authentication = response;	   
+            Backbone.history.navigate("browse", true);
+        };
+        sessionService.makeLogin($("#loginUsername").val(), $("#loginPassword").val());
+    }  
 });
