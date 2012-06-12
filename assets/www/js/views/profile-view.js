b98dab52ffc7daa46e2221c06674c8825ea66869
Anni M Tuulinen <anni.tuulinen@helsinki.fi>
2012-06-12 14:41:04 +0300

    msisdn and organization

--- assets/www/js/views/profile-view.js
+++ assets/www/js/views/profile-view.js
@@ -1,15 +1,14 @@
 App.Views.ProfileView = Backbone.View.extend({
-    render: function() {
-      var user = App.globalUserProfile.get("user");
-      var organization = App.globalUserProfile.get("organization");
-      
-//      alert(JSON.stringify(App.globalUserProfile));
-      var profileElement = Mustache.to_html($("#profile-template").html(), {realName: user.name, MSISDN: user.msisdn, organization: organization.name}); 
-      $(this.el).html(profileElement);
-    },
     
     events: {
 	   "click #logoutButton": "logoutAction"
+    },
+	
+    render: function() {
+      var user = App.globalUserProfile.get("user");
+      var organization = App.globalUserProfile.get("organization");
+      var profileElement = Mustache.to_html($("#profile-template").html(), {realName: user.name, MSISDN: user.msisdn, organization: organization.name}); 
+      $(this.el).html(profileElement);
     },
 
     logoutAction: function() {
