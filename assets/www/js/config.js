30f00931573a972e5e9ea1fe2a55414d948b09b2
Anni M Tuulinen <anni.tuulinen@helsinki.fi>
2012-06-18 14:30:44 +0300

    https

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -1,6 +1,6 @@
 // SnapShopMobile global configuration file
 
-var backendUrl = "http://test.moremr.com/snapshop/api/v1/";
+var backendUrl = "https://test.moremr.com/snapshop/api/v1/";
 var imagesInSet = 12;
 var userSession = {
     authentication: null
